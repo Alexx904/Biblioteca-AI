b8560ea42859954950f263cc7c2b37024dfbbf55
Luca Valenziano <luca.valenziano.lv@gmail.com>
2026-05-25 17:49:09 +0200

    Creazione Componente React NavLink
    Creato il componente riutilizzabile per i collegamenti della navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"; //strumeto per far ricordare react
 import Hamburger from "./Hamburger";
+import NavLink from "./NavLink"
 
 function NavBar(){
 
@@ -13,9 +14,9 @@ function NavBar(){
         <>
             <nav className={`header__nav ${isMenuOpen ? "open" : ""}`} id="headerNav">
                 <ul className="header__nav-list">
-                    <li><a href="index.html#catalogo">Catalogo</a></li>
-                    <li><a href="#postazioni">Postazioni</a></li>
-                    <li><a href="#chat">Chat</a></li>
+                    <NavLink name="Catalogo" link="#catalogo"/>
+                    <NavLink name="Postazioni" link="#postazioni"/>
+                    <NavLink name="Chat" link="#chat"/>
                     <li><button className="buttonNav" id="openPopup">Login / Signup</button></li>
                 </ul>
             </nav>
---
+++ src/NavLink.jsx
@@ -0,0 +1,5 @@
+export default function NavLink(props){
+    return(
+        <li><a href={props.link}>{props.name}</a></li>
+    )
+}
