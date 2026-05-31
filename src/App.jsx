34ab3a4e1206a8dea93dec50d107a737be081855
Luca Valenziano <luca.valenziano.lv@gmail.com>
2026-05-31 19:09:39 +0200

    Piccole modifiche estetiche

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Header from "./Header"
 import Hero from "./Hero";
-import Centrale from "./Centrale";
+import Catalogo from "./Catalogo";
+import Postazioni from "./Postazioni";
+import Chat from "./Chat";
+import Informazioni from "./Informazioni";
 import Footer from "./Footer";
 import {ThemeProvider} from "@mui/material/styles";
 import {lightTheme, darkTheme} from "./Theme";
@@ -15,7 +18,10 @@ function App() {
         <CssBaseline /> {/* CssBaseline è FONDAMENTALE: resetta i margini del browser e applica automaticamente il colore background.default (scuro o chiaro) a tutta la pagina del sito */}
         <Header/>
         <Hero/>
-        <Centrale/>
+        <Catalogo/>
+        <Postazioni/>
+        <Chat/>
+        <Informazioni/>
         <Footer/>
       </ThemeProvider>
     </>
