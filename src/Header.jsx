63073e1dfb77c3ba9677c3e2047cc800e860312f
Dence722 <danielemarzella8@gmail.com>
2026-05-31 18:01:51 +0200

    InserimentoPopupLogin

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,11 @@
 import NavBar from "./NavBar";
-import Auth from "./Auth";
+//import Auth from "./Auth";
 import { Stack,AppBar,Toolbar,Typography, Button, Box, Avatar } from "@mui/material";
 
 function Header(){
     return(
-        /* AppBar con colori personalizzati che gestisce il posizionamento, l'ombra e i colori della Navbar. */
+        <>
+        {/* AppBar con colori personalizzati che gestisce il posizionamento, l'ombra e i colori della Navbar. */}
         <AppBar position="sticky" color="primary">
             {/* Toolbar con justifyContent: 'space-between' per distribuire gli elementi (logo a sinistra, NavLinks al centro, Login a destra). */}
             <Toolbar  sx={{justifyContent: 'space-between', m: 0.5}}>   
@@ -19,17 +20,13 @@ function Header(){
                 </Box>
                 {/* Secondo elemento della NavBar: i NavLinks. */}
                 
-                <NavBar/>                    
-                
-                    {/* Bottone per il pulsante di Login. */}
-                <Button variant="contained" color="secondary" id="openPopup">
-                    Login
-                </Button>
-                
+                <NavBar/>                              
                 
 
             </Toolbar>
         </AppBar>
+        
+        </>
     )
 }
 
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react"; //strumeto per far ricordare react
 import Hamburger from "./Hamburger";
 import NavLink from "./NavLink"
-import { Stack } from "@mui/material";
+import { Dialog, Stack, Box, Typography, Avatar , Menu, MenuItem} from "@mui/material";
+import {Button} from "@mui/material";
+import SignupForm from "./SignupForm";
+import * as React from "react";
+import { MenuBook } from "@mui/icons-material";
 
 function NavBar(){
 
@@ -11,13 +15,106 @@ function NavBar(){
         setIsMenuOpen(!isMenuOpen);
     }
 
+    const [open, setOpen] = React.useState(false);
+
+    const [utenteLoggato, setUtenteLoggato] = useState(null);
+
+    const handleClickOpen = ()=>{
+        setOpen(true);
+    }
+
+    const handleClose = () =>{
+        setOpen(false);
+    }
+
+    // Funzione che scatta quando il login va a buon fine
+    const handleLoginSuccess = (datiUtente) => {
+        setUtenteLoggato(datiUtente); // Salva l'utente
+        setOpen(false); // Chiude il popup
+    };
+
+    const[elenco, setElenco]= useState(null);
+
+    const menuAperto = Boolean(elenco);
+
+    const handleApriMenu = (e)=>{
+        setElenco(e.currentTarget);
+    };
+
+    const handleChiudiMenu = ()=>{
+        setElenco(null);
+    };
+
+    
+    // Funzione per uscire
+    const handleLogout = () => {
+        setUtenteLoggato(null);
+        handleChiudiMenu();
+    };
+
     return(
+        <>
         <Stack direction="row" alignItems="center" spacing={2} sx={{display: { xs: 'none', md: 'flex' }}}> {/*Stack che contiene i NavLink. display none su schermi piccoli, flex su schermi più grandi*/}
             <NavLink name="Catalogo" link="#catalogo"/>
             <NavLink name="Postazioni" link="#postazioni"/>
             <NavLink name="Chat" link="#chat"/>
         </Stack>
-    )
+
+        {/* se utente è loggato mostra Nome e Logout */}
+            {utenteLoggato ? (
+                <>
+                <Box 
+                    onClick={handleApriMenu}
+                    sx={{ 
+                        display: 'flex', 
+                        alignItems: 'center', 
+                        gap: 1.5, 
+                        backgroundColor: '#f0f0f0', 
+                        padding: '5px 15px', 
+                        borderRadius: 5, 
+                        cursor: 'pointer',
+                        transition: '0.3s', 
+                        '&:hover': { backgroundColor: '#e0e0e0' }
+                    }}
+                >
+                    <Avatar sx={{ width: 30, height: 30, bgcolor: 'primary.main' }}>
+                        {utenteLoggato.nome.charAt(0).toUpperCase()}
+                    </Avatar>
+                    <Typography variant="body1" sx={{ fontWeight: 'bold', color: '#1f375e' }}>
+                        {utenteLoggato.nome}
+                    </Typography>
+                </Box>
+
+                <Menu   
+                    anchorEl={elenco}
+                    open={menuAperto}
+                    onClose={handleChiudiMenu}
+                    anchorOrigin={{vertical: "bottom", horizontal:"right"}}
+                    transformOrigin={{vertical: "top", horizontal:"right"}}
+                    >
+                        <MenuItem onClick={handleChiudiMenu}>Le mie Prenotazioni</MenuItem>
+                        <MenuItem onClick={handleChiudiMenu}>Impostazioni</MenuItem>
+                        <MenuItem onClick={handleLogout} sx={{color: "grammar-error.main", fontWeight:"bold"}}>Logout</MenuItem>
+                    </Menu>
+                    </>
+                
+            ) : (
+                /* altrimenti mostra il bottone SignUp/Login */
+                <Button variant="contained" color="secondary" onClick={handleClickOpen}>
+                    SignUp/Login
+                </Button>
+            )}
+
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            maxWidth="xs"
+            fullWidth
+            >
+                <SignupForm onLoginSuccess ={handleLoginSuccess}/>
+            </Dialog>
+        </>
+    );
 
 /*    return(
         <>
